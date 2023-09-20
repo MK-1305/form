dbd6a5eff3ad098a401f2454d202170959265219
松浦一樹 <matsuurakazuki@matsuurakazukinoMacBook-Air.local>
2023-09-20 22:26:22 +0900

    簡易フォーム完成

--- input.php
+++ input.php
@@ -9,7 +9,16 @@
 
     // 入力、確認、完了
 
-    $pageFlag =
+    // 入力画面
+    $pageFlag = 0;
+    // btn_confirmに値が入っていたら
+    if(!empty($_POST['btn_confirm'])){
+        $pageFlag = 1;
+    }
+    // btn_submitに値が入っていたら
+    if(!empty($_POST['btn_submit'])){
+        $pageFlag = 2;
+    }
 
 ?>
 
@@ -22,26 +31,37 @@
 </head>
 <body>
 
-    <?php if($pageFlag === 0) : ?>
-    入力画面
-    <?php endif; ?>
-
     <?php if($pageFlag === 1) : ?>
-    確認画面
+        <form action="input.php" method="post">
+        氏名
+        <?php echo $_POST['your_name']; ?>
+        <br>
+        メールアドレス
+        <?php echo $_POST['email']; ?>
+        <br>
+        <input type="submit" name="back" value="戻る">
+        <input type="submit" name="btn_submit" value="送信する">
+        <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+        <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+
+    </form>
     <?php endif; ?>
 
     <?php if($pageFlag === 2) : ?>
-    完了画面
+    送信が完了しました
     <?php endif; ?>
+
+    <?php if($pageFlag === 0) : ?>
 
     <form action="input.php" method="post">
         氏名
-        <input type="text" name="your_name">
+        <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'];} ?>">
         <br>
-        <input type="checkbox" name="sports[]" value="野球">野球
-        <input type="checkbox" name="sports[]" value="サッカー">サッカー
-        <input type="checkbox" name="sports[]" value="バスケ">バスケ
-        <input type="submit" value="送信">
+        メールアドレス
+        <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'];} ?>">
+        <br>
+        <input type="submit" name="btn_confirm" value="確認する">
     </form>
+    <?php endif; ?>
 </body>
 </html>

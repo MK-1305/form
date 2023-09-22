ea00f70f824e1657fc840ecbfca471076e454ace
松浦一樹 <matsuurakazuki@matsuurakazukinoMacBook-Air.local>
2023-09-22 18:47:42 +0900

    bootstrapでの調整

--- input.php
+++ input.php
@@ -41,6 +41,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -126,29 +127,41 @@
     <?php echo '</ul>' ; ?>
     <?php endif ?>
 
-    <form action="input.php" method="post">
-        氏名
-        <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);} ?>">
-        <br>
-        メールアドレス
-        <!-- バリデーションのテストをするときはnameをtextにしておく -->
-        <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);} ?>">
-        <br>
-        ホームページ
-        <!-- バリデーションのテストをするときはnameをtextにしておく -->
-        <input type="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']);} ?>">
-        <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <form action="input.php" method="post">
+            <div class="form-group">
+                <label for="your_name">氏名</label>
+                <input type="text" class="form-control" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);} ?>" require>
+            </div>
+        <div class="form-group">
+            <label for="email">メールアドレス</label>
+            <!-- バリデーションのテストをするときはnameをtextにしておく -->
+            <input type="email" class="form-control" id="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);} ?>" require>
+        </div>
+        <div class="form-group">
+            <label for="url">ホームページ</label>
+            <!-- バリデーションのテストをするときはnameをtextにしておく -->
+            <input type="url" class="form-control" id="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']);} ?>" require>
+        </div>
         性別
-        <input type="radio" name="gender" value="0"
-        <?php if(isset($_POST['gender']) && $_POST['gender'] === '0')
-        { echo 'checked'; }?>>男性
+        <div class="form-check form-check-inline">
+            <input type="radio" class="form-check-input" id="gender1" name="gender" value="0"
+            <?php if(isset($_POST['gender']) && $_POST['gender'] === '0')
+            { echo 'checked'; }?>>
+            <label class="form-check-label" for="gender1">男性</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input type="radio" class="form-check-input" id="gender2" name="gender" value="1"
+            <?php if(isset($_POST['gender']) && $_POST['gender'] === '1')
+            { echo 'checked'; }?>>
+            <label class="form-check-label" for="gender2">女性</label>
+        </div>
 
-        <input type="radio" name="gender" value="1"
-        <?php if(isset($_POST['gender']) && $_POST['gender'] === '1')
-        { echo 'checked'; }?>>女性
-        <br>
-        年齢
-        <select name="age">
+        <div class="form-group">
+        <label for="age">年齢</label>
+        <select class="form-control" id="age" name="age">
             <option value="">選択してください</option>
             <option value="1">~19歳</option>
             <option value="2">20歳~29歳</option>
@@ -157,16 +170,26 @@
             <option value="5">50歳~59歳</option>
             <option value="6">60歳~</option>
         </select>
-        <br>
-        お問い合わせ内容
-        <textarea name="contact"><?php if(!empty($_POST['contact'])){echo h($_POST['contact']);} ?></textarea>
-        <br>
-        注意事項のチェック
-        <input type="checkbox" name="caution" value="1">注意事項にチェックする
-        <br>
-        <input type="submit" name="btn_confirm" value="確認する">
+        </div>
+
+        <div class="form-group">
+            <label for="contact">お問い合わせ内容</label>
+            <textarea class="form-control" id="contact" row="3" name="contact"><?php if(!empty($_POST['contact'])){echo h($_POST['contact']);} ?></textarea>
+        </div>
+
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="caution" name="caution" value="1">
+            <label class="form-check-label" for="caution">注意事項にチェックする</label>
+        </div>
+
+        <input class="btn btn-info" type="submit" name="btn_confirm" value="確認する">
         <input type="hidden" name="csrf" value="<?php echo $token; ?>">
     </form>
+
+    </div>
+    </div>
+    </div>
     <?php endif; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
